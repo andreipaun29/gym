9eb62b22f7ef3b0c422f36d4f359ea26cc258914
Andrei Paun <andrei.paun1@s.unibuc.ro>
2024-05-11 15:05:11 +0300

    added components

---
+++ src/app/components/voting/voting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voting',
+  standalone: true,
+  imports: [],
+  templateUrl: './voting.component.html',
+  styleUrl: './voting.component.scss'
+})
+export class VotingComponent {
+
+}
